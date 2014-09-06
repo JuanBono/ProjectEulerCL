9669caa7ec8cef22faf638ee0590da608613ba75
Juan Bono <juanbono94@gmail.com>
2014-09-06 12:28:58 -0300

    Create ejer2.lisp

---
+++ ejer2.lisp
@@ -0,0 +1,22 @@
+(defun fibo (n)
+ (cond 
+   ((< n 2) 1) 
+   (T (+ 
+         (fibo (- n 1)) 
+         (fibo (- n 2))
+    ))))
+
+(defun mult2-p (x)
+  (if (= (mod x 2) 0)
+      t
+      nil))
+
+(defun sum-fibo (n)
+  (cond
+    ((= n 0) 0)
+    ((mult2-p (fibo n)) 
+      (+ 
+		   (fibo n) 
+			 (sum-fibo (- n 1))))
+    (T (sum-fibo (- n 1)))
+    ))
